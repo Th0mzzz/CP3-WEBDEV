5aaa527c23ca1c03bce160a048de4b598b5579ce
Thomaz <contatothomazvmendes@gmail.com>
2025-05-22 22:44:52 -0300

    feat: adicionando explicação

--- app.js
+++ app.js
@@ -24,11 +24,13 @@ btnCadastrar.addEventListener("click", (event) => {
         error.textContent = "O preço deve ser maior que zero"
         return;
     }
-    let precoSeparado = preco.toString().split(".")
-    if (precoSeparado[0].length < 1 && precoSeparado[1].length != 2) {
+    let precoSeparado = preco.toString().split(",")
+    if (precoSeparado[0].length < 1 || precoSeparado[1].length != 2) {
         error.textContent = "O preço deve conter duas casas decimais!"
         return;
     }
+
+    // O split separa a string no caracter ',' ,ou seja, separa um número '10,00' em 2 elementos de uma lista. A partir disso, verificamos se o primeiro item dessa lista (precoSeparado[0] ou o numero antes da virgula) tem mais de 1 caracter e se o segundo item (precoSeparado[1] ou o numero depois da virgula) tem mais de 2 caracteres. Se algum desses for falso, ele escreve o erro e termina a função.
 
     const novoProduto = document.createElement('div')
     if (categoria === "tecnologia") {
@@ -36,6 +38,8 @@ btnCadastrar.addEventListener("click", (event) => {
     } else if (categoria === "alimentos") {
         novoProduto.style.backgroundColor = "yellow"
     }
+
+    // style acessa o estilo do elemento div e o altera.
 
     novoProduto.innerHTML = `
     <p>Produto: ${nome}</p>
